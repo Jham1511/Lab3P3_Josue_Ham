a03bc779070cf1a0b0148e67f3c956bae1cc7a41
SKX Kafei <josue.ham@unitec.edu>
2023-05-05 14:30:32 -0600

    Ejercicio 1 casi terminado

--- Lab3P3_Josue_Ham.cpp
+++ Lab3P3_Josue_Ham.cpp
@@ -1,55 +1,108 @@
 #include <iostream>
 using namespace std;
 
+
+
 void discos_ejercicio1() {
 	int disco1[8], disco2[8], discoXOR[8];
-	cout << "1 -> Guardar Datos en XOR" << endl
-		<< "2 -> Obtener datos de disco 1" << endl
-		<< "3 -> Obtener datos de disco 2" << endl
-		<< "4 -> Salir" << endl
-		<< "Ingrese la opcion que desea: ";
-	int opcion;
-	cin >> opcion;
-	switch (opcion) {
-	case 1: {
-		cout << "Ingrese datos del disco 1: " << endl;
-		for (int i = 0; i < 8; i++)
-		{
-			cout << "Disco 1" << "[" << i << "]: ";
-			cin >> disco1[i];
-		}//Este for guarda los valores del disco 1 en posicion i
+	int opcion = 0;
+	bool banderaDisco1 = false;
+	bool banderaDisco2 = false;
+	do
+	{
+		cout << "\n1 -> Guardar Datos en XOR" << endl
+			<< "2 -> Obtener datos de disco 1" << endl
+			<< "3 -> Obtener datos de disco 2" << endl
+			<< "4 -> Salir" << endl
+			<< "Ingrese la opcion que desea: ";
+		cin >> opcion;
+		switch (opcion) {
+		case 1: {
+			banderaDisco1 = true;
+			banderaDisco2 = true;
 
-		cout << "Ingrese datos del disco 2: " << endl;
-		for (int j = 0; j < 8; j++)
-		{
-			cout << "Disco 2" << "[" << j << "]: ";
-			cin >> disco2[j];
-		}
-
-		for (int k = 0; k < 8; k++)
-		{
-			if (disco1[k] == disco2[k])
+			cout << "Ingrese datos del disco 1: " << endl;
+			for (int i = 0; i < 8; i++)
 			{
-				discoXOR[k] = 0;
-			}
-			else if (disco1[k] != disco2[k]) {
-				discoXOR[k] = 1;
-			}
-		} //Este for se encarga de guardar los datos en el disco xor con su metodología
-		cout << "Disco XOR: " << "[";
-		for (int l = 0; l < 8; l++)
-		{
-			cout << discoXOR[l] << " ";
-		}
-		cout << "]";
-	}
-		  break;
-	case 2: {
+				cout << "Disco 1" << "[" << i << "]: ";
+				cin >> disco1[i];
+			}//Este for guarda los valores del disco 1 en posicion i
 
-	}
-		  break;
-	}
-} 
+			cout << "Ingrese datos del disco 2: " << endl;
+			for (int j = 0; j < 8; j++)
+			{
+				cout << "Disco 2" << "[" << j << "]: ";
+				cin >> disco2[j];
+			}//Este for guarda los valores del disco 2 en posicion j
+
+			for (int k = 0; k < 8; k++)
+			{
+				if (disco1[k] == disco2[k])
+				{
+					discoXOR[k] = 0;
+				}
+				else if (disco1[k] != disco2[k]) {
+					discoXOR[k] = 1;
+				}
+			} //Este for se encarga de guardar los datos en el disco xor con su metodología
+			cout << "Disco XOR: " << "[";
+			for (int l = 0; l < 8; l++)
+			{
+				cout << discoXOR[l] << " ";
+			}
+			cout << "]";
+		}
+			  break;
+		case 2: {
+
+			if (banderaDisco1 == true)
+			{	
+				cout << "Disco 1: [";
+				for (int i = 0; i < 8; i++)
+				{
+					if (disco2[i] == discoXOR[i])
+					{
+						cout << "0" << " ";
+					}
+					else {
+						cout << "1" << " ";
+					}
+				}
+				cout << "]";
+			}
+			else {
+				cout << "No se han calculado los valores del disco XOR";
+			}
+		}
+			  break;
+		case 3: {
+			if (banderaDisco2 == true)
+			{	
+				cout << "Disco 2: [";
+				for (int i = 0; i < 8; i++)
+				{
+					if (disco1[i] == discoXOR[i])
+					{
+						cout << "0" << " ";
+					}
+					else {
+						cout << "1" << " ";
+					}
+				}
+				cout << "]";
+			}
+			else {
+				cout << "No se han calculado los valores del disco XOR";
+			}
+		}
+			  break;
+		}
+	} while (opcion!=4);
+}
+
+void matriz_obstaculos() {
+
+}
 int menu() {
 	cout << "\nMenu del Programa" << endl
 		<< "1 -> Ejercicio 1" << endl
@@ -68,6 +121,10 @@ int main()
 		switch (respuesta) {
 		case 1: {
 			discos_ejercicio1();
+		}
+			  break;
+		case 2: {
+
 		}
 			  break;
 		}//Fin del switch
