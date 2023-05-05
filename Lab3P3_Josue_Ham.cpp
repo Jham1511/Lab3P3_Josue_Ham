a4c726c76f2f78fe2d8d2008a77a596bf9761780
SKX Kafei <josue.ham@unitec.edu>
2023-05-05 13:57:19 -0600

    Disco XOR terminado

--- Lab3P3_Josue_Ham.cpp
+++ Lab3P3_Josue_Ham.cpp
@@ -1,19 +1,76 @@
-// Lab3P3_Josue_Ham.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+using namespace std;
+
+void discos_ejercicio1() {
+	int disco1[8], disco2[8], discoXOR[8];
+	cout << "1 -> Guardar Datos en XOR" << endl
+		<< "2 -> Obtener datos de disco 1" << endl
+		<< "3 -> Obtener datos de disco 2" << endl
+		<< "4 -> Salir" << endl
+		<< "Ingrese la opcion que desea: ";
+	int opcion;
+	cin >> opcion;
+	switch (opcion) {
+	case 1: {
+		cout << "Ingrese datos del disco 1: " << endl;
+		for (int i = 0; i < 8; i++)
+		{
+			cout << "Disco 1" << "[" << i << "]: ";
+			cin >> disco1[i];
+		}//Este for guarda los valores del disco 1 en posicion i
+
+		cout << "Ingrese datos del disco 2: " << endl;
+		for (int j = 0; j < 8; j++)
+		{
+			cout << "Disco 2" << "[" << j << "]: ";
+			cin >> disco2[j];
+		}
+
+		for (int k = 0; k < 8; k++)
+		{
+			if (disco1[k] == disco2[k])
+			{
+				discoXOR[k] = 0;
+			}
+			else if (disco1[k] != disco2[k]) {
+				discoXOR[k] = 1;
+			}
+		} //Este for se encarga de guardar los datos en el disco xor con su metodología
+		cout << "Disco XOR: " << "[";
+		for (int l = 0; l < 8; l++)
+		{
+			cout << discoXOR[l] << " ";
+		}
+		cout << "]";
+	}
+		  break;
+	case 2: {
+
+	}
+		  break;
+	}
+} 
+int menu() {
+	cout << "\nMenu del Programa" << endl
+		<< "1 -> Ejercicio 1" << endl
+		<< "2 -> Ejercicio 2" << endl
+		<< "3 -> Salir" << endl
+		<< "Ingrese la opcion que desea: " << endl;
+	int numero;
+	cin >> numero;
+	return numero;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int respuesta = menu();
+	while (respuesta != 3) {
+		switch (respuesta) {
+		case 1: {
+			discos_ejercicio1();
+		}
+			  break;
+		}//Fin del switch
+	}//Fin del while
+	respuesta = menu();
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
